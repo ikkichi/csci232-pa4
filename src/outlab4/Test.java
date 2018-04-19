5a2b0b78814104f55b1e2c9cf6d76b0d83d1a2c1
ikkichi <36936847+ikkichi@users.noreply.github.com>
2018-04-19 17:07:00 -0600

    Added @Test methods and JUnit imports

--- src/outlab4/Test.java
+++ src/outlab4/Test.java
@@ -7,5 +7,36 @@
 
 package outlab4;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class Test {
+
+    @Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testSomeMethodEmptyArray() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testSomeMethodExtra1() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testSomeMethodExtra2() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
 }
