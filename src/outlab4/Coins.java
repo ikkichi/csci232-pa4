83f3dc35dc2a2e6c8a0c0c9aab27c48a535bde9b
mathmaster12 <36994858+mathmaster12@users.noreply.github.com>
2018-04-24 20:50:09 -0600

    Update Coins.java

--- src/outlab4/Coins.java
+++ src/outlab4/Coins.java
@@ -8,54 +8,52 @@
 package outlab4;
 
 public class Coins {
-    	  public static int[] makeChange(int[] coinsAvailable, int change, int[] list, int[] coinsGiven){
-			int x = 0;
-			while(x < list.length){
-				list[x] = 0;
-				coinsGiven[x] = 0;
-				++x;
-			}
-			if(coinsAvailable.length <= 0) {
-				throw new IllegalArgumentException("Array of size 0 is not allowed");
-			}
-			if(change <= 0){
-				throw new IllegalArgumentException("Change must be a positive nonzero number"); 
-			}
-			int z = 0; 
-			for(int j = 0; j < coinsAvailable.length; ++j){
-				for(int w = 0; w < coinsAvailable.length; ++w){
-					if(coinsAvailable[j] == 0){
-						return null; 
-					}
-					if(coinsAvailable[j] == Integer.MAX_VALUE){
-						return null;
-					}
-					if(coinsAvailable[j] <= change){
-						list[z] = coinsAvailable[j];
-						change = change-coinsAvailable[j];
-						++z;
-					}
-				}
-				++j;
-			} 
-			int w = 0;
-			System.out.println("Coins needed for change: ");
-			while(w < list.length){
-				if(list[w] != 0){
-					System.out.print(list[w]+" ");
-				}
-				++w;
-			}
-			System.out.println("");
-			return list; 
+  	public static int[] makeChange(int[] coins, int change){
+		int[] list = new int[change]; 
+		int x = 0;
+		while(x < list.length){
+			list[x] = 0;
+			++x;
 		}
-	
+		if(coins.length == 0) {
+			throw new IllegalArgumentException("Array of size 0 is not allowed");
+		}
+		int i = 0;
+		int y = 0; 
+		while(i < coins.length){
+			for(int v=0;v < list.length; ++v){
+			if(coins[i] <= change){
+				list[y] = coins[i];
+				change = change-coins[i];
+				++y;
+			}
+			if(coins[i] == 0){
+				return null; 
+			}
+			if(coins[i] == Integer.MAX_VALUE){
+				return null;
+			}
+			}
+			++i;
+		}
+		System.out.println("Coins needed are: ");
+		int z = 0;
+		while(z < list.length){
+			if(list[z] != 0){
+				System.out.print(list[z]+",");
+			}
+			++z;
+		}
+		return list; 
+	}
 	/*public static void main(String[] args){
-		int[] coins = {25, 10, 5, 1}; 
-		int change = 24;
-		int[] minimumCoins = new int[change+1]; 
-		int[] usedCoins = new int[change+1];
-		int[] give;
-		give = makeChange(coins, change, minimumCoins, usedCoins);
+		int[] coins = new int[4];
+		coins[0] = 25;
+		coins[1] = 10;
+		coins[2] = 5;
+		coins[3] = 1; 
+		int change = 40;
+		int[] give = new int[change];
+		give = makeChange(coins,change);
 	} */
 }
